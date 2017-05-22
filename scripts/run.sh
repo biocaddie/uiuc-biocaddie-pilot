a56c495948c092eaa30418e030f6672c016e9bfa
Craig Willis <willis8@illinois.edu>
2017-05-22 15:24:10 +0000

    Added LD_LIBRARY_PATH export to run.sh and stoplist.all

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,8 @@ then
    IRUTILS_HOME=.
 fi
 
+export LD_LIBRARY_PATH=/usr/local/lib
 export _SILENT_JAVA_OPTIONS=-Xmx4g
 unset _JAVA_OPTIONS
 #java -Xmx16g -Djava.library.path=~/lib/ -cp "lib/*" $@
-java -cp "target/biocaddie-0.0.1-SNAPSHOT.jar:target/lib/*:lib/*" $@
+java  -cp "target/biocaddie-0.0.1-SNAPSHOT.jar:target/lib/*:lib/*" $@
