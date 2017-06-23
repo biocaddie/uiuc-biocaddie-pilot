a35d90a1b196621eec76af12ecc7ee3a9fd8ab12
Mike Lambert <lambert8@illinois.edu>
2017-06-23 16:34:45 -0500

    Simplify logging, adjust lease_secs, and some better error handling on the subprocess call

--- worker.py
+++ worker.py
@@ -15,18 +15,9 @@ output_dir='/output/' + queue.replace('-', '/')
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-# create formatter and add it to the log handlers
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-# Set up a console logger
-logger = logging.getLogger(queue)
-logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-ch.setFormatter(formatter)
-
-# create formatter and add it to the handlers
-logger.addHandler(ch)
+# Set up a basic file logger
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging
 
 q = rediswq.RedisWQ(name=queue, host=host)
 logger.info("Worker started with sessionID: {0}".format(q.sessionID()))
@@ -34,17 +25,20 @@ logger.debug("Initial state: empty={0}".format(str(q.empty())))
 
 start_time = time.time()
 while not q.empty():
-  item = q.lease(lease_secs=10, block=True, timeout=2) 
+  item = q.lease(lease_secs=200, block=True, timeout=2) 
   if item is not None:
     # Read a work item (a bash command) from the queue
     command = item.decode("utf=8")
     logger.info("Running command: {0}".format(command))
 
-    # Execute the command given by the queue item
-    output = subprocess.check_output(['bash', '-c', command], shell=False) 
+    try:
+        # Execute the command given by the queue item
+        output = subprocess.check_output(['bash', '-c', command], shell=False) 
+        # Mark the item as complete in the queue
+        q.complete(item)
+    except CalledProcessError as ex:
+        logger.error("ERROR: {0} - {1}".format(ex.returnCode, ex.output))
 
-    # Mark the item as complete in the queue
-    q.complete(item)
   else:
     logger.debug("Waiting for work")
 logger.debug("Queue is empty, exiting")
